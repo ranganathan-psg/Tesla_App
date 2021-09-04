d7877d4320b48258db2f905218e450f67b40d1fb
Ranganathan <89693544+ranganathan-psg@users.noreply.github.com>
2021-09-04 13:24:02 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tesla App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         // This is the theme of your application.
